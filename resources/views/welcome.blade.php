bcc013e080b0df54ef7fc1ce06bc0278b0ac13cd
ghassen <ghassenahmed74@gmail.com>
2023-01-19 17:26:56 +0100

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,15 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Simple CRUD</title>
 </head>
+<style>
+    h2{
+        color: #13005A;
+        font-family: Verdana, Geneva, Tahoma, sans-serif;
+        font-size: 20px;
+        text-align: center;
+    }
+</style>
 <body>
-    
+    <h2>Simple CRUD </h2>
 </body>
 </html>
