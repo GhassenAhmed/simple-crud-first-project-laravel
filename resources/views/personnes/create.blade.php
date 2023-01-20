bc46b1216fcf9e4875a0407ed234e6bf6ca298a7
ghassen <ghassenahmed74@gmail.com>
2023-01-20 17:15:01 +0100

    add inputs

--- resources/views/personnes/create.blade.php
+++ resources/views/personnes/create.blade.php
@@ -18,11 +18,43 @@
         font-size: 50px;
         text-align: center;
     }
-    
+    /* From uiverse.io by @alexruix */
+.input {
+ line-height: 28px;
+ border: 2px solid transparent;
+ border-bottom-color: #777;
+ padding: .2rem 0;
+ outline: none;
+ background-color: transparent;
+ color: #0d0c22;
+ transition: .3s cubic-bezier(0.645, 0.045, 0.355, 1);
+}
+
+.input:focus, input:hover {
+ outline: none;
+ padding: .2rem 1rem;
+ border-radius: 1rem;
+ border-color: #7a9cc6;
+}
+
+.input::placeholder {
+ color: #777;
+}
+
+.input:focus::placeholder {
+ opacity: 0;
+ transition: opacity .3s;
+}    
 </style>
 <body>
     <div class="container mt-5">
         <h2 >Add New Person</h2>
+
+        <div class="inputs">
+            <input placeholder="Username" type="text" class="input" required="">
+            <input placeholder="Username" type="text" class="input" required="">
+            <input placeholder="Username" type="text" class="input" required="">
+        </div>
     </div>
     
 </body>
